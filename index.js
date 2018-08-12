8b8a6d1a8f380fab1d95d20c4ec90f6fe2b699b7
jasonacole82@gmail.com <jasonacole82@gmail.com>
2018-08-12 20:24:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log('HELLO')
+  console.log('HELLO!')
 }
 
 function logWhisper(string) {
-  console.log('hello')
+  console.log('hello!')
 }
 
 function sayHiToGrandma(string) {
-  var uppercase = "HELLO"
+  var uppercase = "HELLO!"
   uppercase.toUpperCase() === uppercase
   return "YES INDEED!"
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = "hello"
+  var lowercase = "hello!"
   lowercase.toLowerCase() === lowercase
   return "I can't hear you!"
 }
