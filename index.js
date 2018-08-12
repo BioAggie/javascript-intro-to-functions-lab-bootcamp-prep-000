ef73bddbfea4ccf9f14cb830ede87d73c569de7d
jasonacole82@gmail.com <jasonacole82@gmail.com>
2018-08-12 20:29:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var lowercase = "hello"
   lowercase.toLowerCase() === lowercase
-  return "I can't hear you!"
+  return "YES INDEED!"
 }
 
 function sayHiToGrandma(string) {
